f9cfb5e1824d7d0235bc679bf7d1c86416f5d4af
tricao1212 <tri.cao.cit20@eiu.edu.vn>
2024-04-23 15:15:06 +0700

    addPropertyModel

---
+++ src/PropertyOwner.java
@@ -0,0 +1,17 @@
+
+public class PropertyOwner {
+	private User user;
+	private List<Property> listProperties;
+
+	public getUser(){
+	}
+	
+	public getListProperty(){
+	}
+	
+	public void addProperty(){
+	}
+	
+	public void removeProperty(){
+	}
+}
